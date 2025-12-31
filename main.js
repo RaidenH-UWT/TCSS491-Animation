58458f0e1c80ebb97497fc97d1a62d000c63365e
RaidenH-UWT <raidenh@uw.edu>
2025-12-31 02:12:59 -0800

    github pages are weird about resource locations

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("/icon.png");
-ASSET_MANAGER.queueDownload("/sprites/spyro_spritesheet.png");
+ASSET_MANAGER.queueDownload("./icon.png");
+ASSET_MANAGER.queueDownload("./sprites/spyro_spritesheet.png");
 
 ASSET_MANAGER.downloadAll(startGame);
 
